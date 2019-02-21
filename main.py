2b9fffeff0f0988cdf3cb9ebd5dde30391481fb9
kamil.jerzy.wojcik <kamil.jerzy.wojcik@gmail.com>
2019-02-21 14:36:33 +0100

    data

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import numpy
-import pandas
-import sklearn
-import matplotlib.pyplot as pyplot
+import os
+import tarfile
+from six.moves import urllib
+
+# import numpy
+# import pandas
+# import sklearn
+# import matplotlib.pyplot as pyplot
+# import scipy
 
 print("Hello python")
